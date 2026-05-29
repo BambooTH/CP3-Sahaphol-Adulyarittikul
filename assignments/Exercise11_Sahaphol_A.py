fe2b8fcdc67e1b6a12f5d8dc409c2018159bcc7e
BambooTH <bambooadulth@gmail.com>
2026-05-29 15:51:08 +0700

    Update Exercise11_Sahaphol_A.py

--- assignments/Exercise11_Sahaphol_A.py
+++ assignments/Exercise11_Sahaphol_A.py
@@ -1,5 +1,3 @@
 Number = int(input("Enter Number: "))
 for x in range(Number):
-    star = " "*(Number - x -1)
-    space = "*"*(2* x + 1)
-    print(star + space)
+    print(" "*(Number - x -1) + "*"*(2 * x + 1))
