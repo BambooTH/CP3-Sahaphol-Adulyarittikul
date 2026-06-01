91bd6ffa656a941e2c312593d4842069e5caafc9
BambooTH <bambooadulth@gmail.com>
2026-06-01 14:08:40 +0700

    Create Function50_Sahaphol_A.py

---
+++ assignments/Function50_Sahaphol_A.py
@@ -0,0 +1,12 @@
+def plus(x,y):
+    print(x+y)
+def minus(x,y):
+    print(x-y)
+def multiply(x,y):
+    print(x*y)
+def divide(x,y):
+    print(x/y)
+plus(6,7)
+minus(6,7)
+multiply(6,7)
+divide(7,6)
